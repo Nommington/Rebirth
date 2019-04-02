cf28b987c6ff3e1a6ed9eb3407869e9e2ac54291
Nommington <rymckenz@gmail.com>
2019-04-02 15:10:45 -0400

    Started JS

---
+++ 05-birthday/assets/logic.js
@@ -0,0 +1,15 @@
+var pics = ["images/born.jpg", "images/firstMet.jpg", "images/firstDate.jpg", "images/dance.jpg", "images/wedding.jpg"];
+var subt = [
+    "Today is the day you were born",
+    "And from the day we met",
+    "to our first date",
+    "to our first dance",
+    "to the day we started this crazy thing",
+    "to the day we made a family",
+    "to the day our family was complete",
+];
+
+function openCard() {
+    var page = -1;
+    
+}
